5f889e1eb64e353aecc7a2da71cd89927b5b7297
RectoO <nicosvrielynck@hotmail.com>
2015-10-07 11:02:54 +0200

    debut validation formulaire d'inscription

--- ASMAE/tennis/static/tennis/js/tennis.js
+++ ASMAE/tennis/static/tennis/js/tennis.js
@@ -1,2 +1,129 @@
 document.getElementById("foot01").innerHTML =
 "<p>&copy;  " + new Date().getFullYear() + " ASMAE. All rights reserved.</p>";
+
+/*
+* Validation du formulaire d'inscription
+* Vérification des champs et de la validité du numero de tel email et addresse
+* TODO : email adress et tel validity
+*/
+function validateRegister() {
+	var valid = true;
+
+	//Verification username présent et > 2
+	var username = document.forms["registerForm"]["username"].value;
+	if(username==null || username == ""){
+		document.getElementById("hint-username").innerHTML = " ! Entrer un nom d'utilisateur";
+		valid = false;
+	}else{
+		if(username.length<2){
+			document.getElementById("hint-username").innerHTML = " ! Votre nom d'utilisateur doit contenir au moins 3 caractères";
+			valid = false;
+		}else{
+			document.getElementById("hint-username").innerHTML = "";
+		}
+	}
+
+	//Verification mot de passe présent et > 2
+	var password = document.forms["registerForm"]["password"].value;
+	if(password==null || password == ""){
+		document.getElementById("hint-password").innerHTML = " ! Entrer un mot de passe";
+		valid = false;
+	}else{
+		if(password.length<2){
+			document.getElementById("hint-password").innerHTML = " ! Votre mot de passe doit contenir au moins 3 caractères";
+			valid = false;
+		}else{
+			document.getElementById("hint-password").innerHTML = "";
+		}
+		document.getElementById("hint-password").innerHTML = "";
+	}
+
+	//Verification nom présent
+	var lastname = document.forms["registerForm"]["lastname"].value;
+	if(lastname==null || lastname == ""){
+		document.getElementById("hint-lastname").innerHTML = " ! Entrer votre nom";
+		valid = false;
+	}else{
+		document.getElementById("hint-lastname").innerHTML = "";
+	}
+
+	//Verification prénom présent
+	var firstname = document.forms["registerForm"]["firstname"].value;
+	if(firstname==null || firstname == ""){
+		document.getElementById("hint-firstname").innerHTML = " ! Entrer votre prénom";
+		valid = false;
+	}else{
+		document.getElementById("hint-firstname").innerHTML = "";
+	}
+
+	//Verification email présent
+	var email = document.forms["registerForm"]["email"].value;
+	if(email==null || email == ""){
+		document.getElementById("hint-email").innerHTML = " ! Entrer un email valide";
+		valid = false;
+	}else{
+		//Verify email validity TODO
+		document.getElementById("hint-email").innerHTML = "";
+	}
+
+	//Verification telephone présent
+	var tel = document.forms["registerForm"]["tel"].value;
+	var gsm = document.forms["registerForm"]["gsm"].value;
+	if((tel==null || tel == "") && (gsm==null || gsm == "")){
+		document.getElementById("hint-tel").innerHTML = " ! Entrer votre numéro de téléphone ou GSM";
+		valid = false;
+	}else{
+		//Verify number validity TODO
+		document.getElementById("hint-tel").innerHTML = "";
+	}
+
+	//Verification rue présent
+	var street = document.forms["registerForm"]["street"].value;
+	if(street==null || street == ""){
+		document.getElementById("hint-street").innerHTML = " ! Entrer votre rue";
+		valid = false;
+	}else{
+		document.getElementById("hint-street").innerHTML = "";
+	}
+
+	//Verification numero présent
+	var number = document.forms["registerForm"]["number"].value;
+	if(number==null || number == ""){
+		document.getElementById("hint-number").innerHTML = " ! Entrer votre numéro/boite";
+		valid = false;
+	}else{
+		document.getElementById("hint-password").innerHTML = "";
+	}
+
+	//Verification code postal présent
+	var postalcode = document.forms["registerForm"]["postalcode"].value;
+	if(postalcode==null || postalcode == ""){
+		document.getElementById("hint-postalcode").innerHTML = " ! Entrer votre code postal";
+		valid = false;
+	}else{
+		document.getElementById("hint-postalcode").innerHTML = "";
+	}
+
+	//Verification localité présent
+	var locality = document.forms["registerForm"]["locality"].value;
+	if(locality==null || locality == ""){
+		document.getElementById("hint-locality").innerHTML = " ! Entrer votre localité";
+		valid = false;
+	}else{
+		document.getElementById("hint-locality").innerHTML = "";
+	}
+
+	//TODO vérification addresse correcte
+
+	//Verification date de naissance présent
+	var birthdate = document.forms["registerForm"]["birthdate"].value;
+	if(birthdate==null || birthdate == ""){
+		document.getElementById("hint-birthdate").innerHTML = " ! Entrer votre date de naissance";
+		valid = false;
+	}else{
+		document.getElementById("hint-birthdate").innerHTML = "";
+	}
+
+
+	return valid;
+}
--- ASMAE/tennis/views.py
+++ ASMAE/tennis/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from tennis.forms import LoginForm
 
@@ -45,7 +46,71 @@ def deconnect(request):
 	logout(request)
 	return redirect(reverse(home))
 
+def username_present(username):
+	if User.objects.filter(username=username).exists():
+		return True
+
+	return False
+
+def email_present(email):
+    if User.objects.filter(email=email).exists():
+        return True
+
+    return False
+
 def register(request):
+	if request.method == "POST":
+
+		#Recuperation des donnees
+		username = request.POST['username']
+		password = request.POST['password']
+		firstname = request.POST['firstname']
+		lastname = request.POST['lastname']
+		email = request.POST['email']
+		tel = request.POST['tel']
+		gsm = request.POST['gsm']
+		street = request.POST['street']
+		number = request.POST['number']
+		locality = request.POST['locality']
+		postalcode = request.POST['postalcode']
+		birthdate = request.POST['birthdate']
+		classement = request.POST['classement']
+		#participated = request.POST['participated']
+
+
+		#check champs
+		if (username=="" or password=="" or firstname=="" or lastname=="" or email=="" or tel=="" 
+		or gsm=="" or street=="" or number=="" or locality=="" or postalcode=="" or birthdate==""):
+			error = "Veuillez remplir tous les champs obligatoires !"
+			return render(request,'tennis/register.html',locals())
+
+		#Check username et email already taken
+		if(username_present(username)):
+			error = "Cet nom d'utilisateur n'est plus disponible !"
+			return render(request,'tennis/register.html',locals())
+
+		if(email_present(email)):
+			error = "Un compte avec cette addresse email existe déjà !"
+			return render(request,'tennis/register.html',locals())
+
+		#check username length
+		if(len(username) < 2):
+			error = "Votre nom d'utilisateur doit contenir au moins 3 caractères"
+			return render(request,'tennis/register.html',locals())
+
+		#check password length
+		if(len(password) < 2):
+			error = "Votre mot de passe doit contenir au moins 3 caractères"
+			return render(request,'tennis/register.html',locals())
+
+		#Check numero de tel addresse et email valide TODO
+
+
+
+		#Account creation & redirect
+		
+	if request.user.is_authenticated():
+		return redirect(reverse(home))
 	return render(request,'tennis/register.html',locals())
 
 def recover(request):
