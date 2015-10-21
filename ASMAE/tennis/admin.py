ffbda3699bd26c3619c0b4e9bcb2f18af365383f
QuentinG3 <QG3393@hotmail.com>
2015-10-21 15:31:37 +0200

    modif

--- ASMAE/tennis/admin.py
+++ ASMAE/tennis/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from tennis.models import Extra,Participant
+from tennis.models import Extra,Participant,Court
 # Register your models here.
 admin.site.register(Participant)
 admin.site.register(Extra)
+admin.site.register(Court)
