c60b8c9ab128eb9e7582f2a158cb01bfa48a1d41
= <florian.thuin@student.uclouvain.be>
2015-11-04 20:52:50 +0100

    modif affichage django admin

--- ASMAE/tennis/admin.py
+++ ASMAE/tennis/admin.py
@@ -1,9 +1,28 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 from tennis.models import Extra,Participant,Court,Tournoi,Pair,Groupe
 # Register your models here.
-admin.site.register(Participant)
-admin.site.register(Extra)
+
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'nom', 'prenom', 'rue', 'numero', 'codepostal', 'telephone', 'fax', 'gsm', 'datenaissance', 'classement', 'oldparticipant', 'localite')
+    list_display_links = ('nom',)
+    list_filter = ('titre',)
+    ordering = ('nom',)
+    search_fields = ('titre', 'nom', 'prenom', 'rue', 'numero', 'codepostal', 'telephone', 'fax', 'gsm', 'datenaissance', 'classement', 'oldparticipant', 'localite')
+    
+class ExtraAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'prix')
+    ordering = ('nom',)
+    search_fields = ('nom', 'prix', 'commentaires')
+    list_editable = ('prix',)
+    
+class PairAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user1', 'user2', 'confirm', 'valid', 'pay')
+    ordering = ('id',)
+    
+admin.site.register(Participant, ParticipantAdmin)
+admin.site.register(Extra, ExtraAdmin)
 admin.site.register(Court)
 admin.site.register(Tournoi)
-admin.site.register(Pair)
+admin.site.register(Pair, PairAdmin)
 admin.site.register(Groupe)
