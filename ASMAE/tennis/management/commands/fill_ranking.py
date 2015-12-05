9873ac5f3abde47e30b24a879f7c69b7b018fe65
Florian Thuin <florian.thuin@student.uclouvain.be>
2015-12-05 18:15:35 +0100

    corr fill_ranking

--- ASMAE/tennis/management/commands/fill_ranking.py
+++ ASMAE/tennis/management/commands/fill_ranking.py
@@ -12,7 +12,7 @@ import datetime
 list_rankings = ["NC", "C30.5", "C30.4", "C30.3", "C30.2", "C30.1", "C30", "C15.5", "C15.4", "C15.3", "C15.2", "C15.1", "C15", "B+4/6", "B+2/6", "B0"]
 
 class Command(BaseCommand):
-    def addRankings():
+    def addRankings(self):
         for elem in list_rankings:
             r = Ranking(nom=elem)
             r.save()
