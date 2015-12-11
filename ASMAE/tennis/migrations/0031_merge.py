7e9a0046c3c21584f26612bb52e3406c07346cc9
Florian Thuin <florian.thuin@student.uclouvain.be>
2015-12-11 21:35:39 +0100

    db avec scores pour poule de 16

---
+++ ASMAE/tennis/migrations/0031_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tennis', '0030_auto_20151210_1227'),
+        ('tennis', '0030_auto_20151209_1445'),
+    ]
+
+    operations = [
+    ]
